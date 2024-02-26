72ec1a7ac8379a276cbd126a7f12d2913019282c
medineshkumarm <medineshkumarm@gmail.com>
2024-02-27 00:41:18 +0530

    regsiter to gform

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -3,15 +3,12 @@ import React, { useEffect } from "react";
 const Register: React.FC = () => {
   useEffect(() => {
     // Redirect to the specific link when the component mounts
-    window.location.href = "https://forms.gle/GbcXhB3JLxmZHnL36";
+    window.location.href = "https://forms.gle/sQGS5WEiKAfYqdyC6";
   }, []);
 
   // You can render some content here
   return (
-    <div>
-      <p>Redirecting to the registration page...</p>
-      {/* You can add more content if needed */}
-    </div>
+ null
   );
 };
 
